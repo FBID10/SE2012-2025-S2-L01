3449c20cf6728e05637f8a5b1ea2ed95394ab46f
Black Horse <FBIDileepa10@gmail.com>
2025-07-28 19:35:07 +0530

    Added Welcome.java file

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Welcome{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your First Name");
+		String firstName=sc.nextLine();
+		System.out.println("Enter your Last Name");
+		String lastName=sc.nextLine();
+		System.out.printf("Welcome to the Second Year %s %s",firstName,lastName);
+
+	}
+
+}
